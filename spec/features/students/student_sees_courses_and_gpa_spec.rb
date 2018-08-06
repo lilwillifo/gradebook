fc604c80fbd5b55eeff10ac0dde89cd6524b5a15
Margaret Williford <margaretwilliford4@gmail.com>
2018-08-06 14:31:26 -0600

    student sees their courses and grades on their dashboard

---
+++ spec/features/students/student_sees_courses_and_gpa_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe "Student views their dashboard" do
+  scenario "and sees all their courses and grades" do
+    student = create(:student)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(student)
+    student = Student.find_by(user_id: student)
+    teacher = create(:teacher)
+    teacher = Teacher.find_by(user_id: teacher)
+    course = teacher.courses.create(title: 'Software Engineering')
+    other_course = Course.create(title: 'Art History')
+    Enrollment.create!(student: student, course: course, grade: 3.7)
+
+    visit dashboard_path
+
+    expect(current_path).to eq("/dashboard")
+    expect(page).to have_content(course.title)
+    expect(page).to_not have_content(other_course.title)
+  end
+  scenario "and teachers cannot see all courses a student is registered for" do
+
+  end
+end
