bb04ccbfe79d2751cc76be7423514055a4c1dc55
Margaret Williford <margaretwilliford4@gmail.com>
2018-08-07 08:25:53 -0600

    teacher receives flash message if they try to add a student that is already enrolled in a course

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,14 +1,19 @@
 class EnrollmentsController < ApplicationController
   def new
+    @students = Student.all
   end
 
   def create
     student = Student.find(params[:student][:id])
     course = Course.find(params[:course_id])
-    Enrollment.create(course: course, student: student)
-
-    flash[:message] = "Added #{student.name} to #{course.title}"
-    redirect_to course_path(params[:course_id])
+    if Enrollment.find_by(course: course, student: student)
+      flash[:message] = "#{student.name} is already enrolled."
+      redirect_to new_course_enrollment_path
+    else
+      Enrollment.create(course: course, student: student)
+      flash[:message] = "Added #{student.name} to #{course.title}"
+      redirect_to course_path(params[:course_id])
+    end
   end
 
   def edit
--- spec/features/teachers/teacher_can_manage_her_roster_spec.rb
+++ spec/features/teachers/teacher_can_manage_her_roster_spec.rb
@@ -6,7 +6,7 @@ describe "Teacher manages their course roster" do
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
     teacher = Teacher.find_by(user_id: user)
     semester = Semester.create(session: 'fall', year: 2018)
-    course = teacher.courses.create(title: 'Software Engineering', semester:semester)
+    course = teacher.courses.create(title: 'Software Engineering', semester: semester)
     user_2 = create(:student)
     student = Student.find_by(user_id: user_2)
 
@@ -19,6 +19,13 @@ describe "Teacher manages their course roster" do
     expect(page).to have_content("Added #{student.name} to #{course.title}")
     expect(student.courses).to eq([course])
     expect(course.students).to eq([student])
+
+    visit new_course_enrollment_path(course)
+    select student.name, :from => "student_id"
+    click_button "Add"
+
+    expect(current_path).to eq (new_course_enrollment_path(course))
+    expect(page).to have_content "#{student.name} is already enrolled."
   end
   scenario "a teacher can remove a student from their roster" do
     user = create(:teacher)
