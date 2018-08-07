3ec8787a05a794856d7ab01be9c18a5f2900106e
Margaret Williford <margaretwilliford4@gmail.com>
2018-08-06 19:12:00 -0600

    admin can sort by semesters

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,13 +3,17 @@
 end
 
 teachers = (1..5).map do |n|
-  User.create(username: "teacher#{n}", password: "test", role: 1)
+  User.create!(username: "teacher#{n}", password: "test", role: 1)
 end
 
-User.create(username: "Admin", password: "test", role: 2)
+User.create!(username: "Admin", password: "test", role: 2)
+
+fall = Semester.create!(session: 'fall', year: 2017)
+spring = Semester.create!(session: 'spring', year: 2018)
+summer = Semester.create!(session: 'summer', year: 2018)
 
 teachers.each_with_index do |teacher, index|
-  teacher.teacher.courses.create(title: "Course#{index}")
+  teacher.teacher.courses.create!(title: "Course#{index}", semester: Semester.order("RANDOM()").first)
 end
 
 15.times do
